e1ba703759e6e0a04cbb5a417da3eebc8b155077
guiinoyaruga <willfkrause@outlook.com>
2026-03-28 12:46:55 -0300

    feat: script git hub api added

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  allowCypressEnv: true,
+
+  env:{
+      github_token: "",
+      api_url: 'https://api.github.com'
+    },
 
   e2e: {
     baseUrl: 'https://nexdom.tec.br/',
     
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
   viewportWidth: 1280,
---
+++ cypress/e2e/validar-api-github.cy.js
@@ -0,0 +1,130 @@
+import { fakerPT_BR as faker } from '@faker-js/faker'
+
+describe('GitHub API', () => {
+    const randomNumber = faker.string.numeric(6)
+    const randomText = faker.lorem.lines(1)
+    const randomTextTitle = faker.lorem.lines(1)
+    const randomTextBody = faker.lorem.lines(1)
+    let idIssue = 0
+
+    it('Deve ser possivel criar um repositorio no Github', () => {
+        cy.env(['github_token', 'api_url'])
+            .then(({ github_token, api_url }) => {
+                cy.request({
+                    method: 'POST',
+                    url: `${api_url}/user/repos`,
+                    headers: {
+                        Authorization: `Bearer ${github_token}`,
+                        Accept: 'application/vnd.github+json'
+                    },
+                    body: {
+                        name: `repository-${randomNumber}`,
+                        description: `${randomText}`,
+                        private: false
+                    }
+                }).then((response) => {
+                    expect(response.status).to.equal(201)
+                })
+            })
+    });
+
+    it('Deve ser possivel pesquisar por um repositório já criado no Github', () => {
+        cy.env(['github_token', 'api_url'])
+            .then(({ github_token, api_url }) => {
+                cy.request({
+                    method: 'GET',
+                    url: `${api_url}/repos/guiinoyaruga/repository-${randomNumber}`,
+                    headers: {
+                        Authorization: `Bearer ${github_token}`,
+                        Accept: 'application/vnd.github+json'
+                    },
+                }).then((response) => {
+                    const repositoryName = response.body.name
+                    const repositoryDescription = response.body.description
+                    const repositoryisNotPrivate = response.body.private
+
+                    expect(response.status).to.equal(200)
+                    expect(repositoryName).to.equal(`repository-${randomNumber}`)
+                    expect(repositoryDescription).to.equal(`${randomText}`)
+                    expect(repositoryisNotPrivate).to.be.false
+                })
+            })
+    });
+
+    it('Deve ser possivel criar uma issue em um repositório do Github', () => {
+        cy.env(['github_token', 'api_url'])
+            .then(({ github_token, api_url }) => {
+                cy.request({
+                    method: 'POST',
+                    url: `${api_url}/repos/guiinoyaruga/repository-${randomNumber}/issues`,
+                    headers: {
+                        Authorization: `Bearer ${github_token}`,
+                        Accept: 'application/vnd.github+json'
+                    },
+                    body: {
+                        title: `${randomTextTitle}`,
+                        body: `${randomTextBody}`,
+                    }
+                }).then((response) => {
+                    expect(response.status).to.equal(201)
+                    idIssue = response.body.number
+                })
+            })
+    });
+
+    it('Deve ser possivel buscar por uma issue em um repositório do Github', () => {
+        cy.env(['github_token', 'api_url'])
+            .then(({ github_token, api_url }) => {
+                cy.request({
+                    method: 'GET',
+                    url: `${api_url}/repos/guiinoyaruga/repository-${randomNumber}/issues/${idIssue}`,
+                    headers: {
+                        Authorization: `Bearer ${github_token}`,
+                        Accept: 'application/vnd.github+json'
+                    },
+                }).then((response) => {
+                    const issueTitle = response.body.title
+                    const issueBody = response.body.body
+
+                    expect(response.status).to.equal(200)
+                    expect(issueTitle).to.equal(`${randomTextTitle}`)
+                    expect(issueBody).to.equal(`${randomTextBody}`)
+                })
+            })
+    });
+
+    it('Deve ser possivel deletar um repositório do Github', () => {
+        cy.env(['github_token', 'api_url'])
+            .then(({ github_token, api_url }) => {
+                cy.request({
+                    method: 'DELETE',
+                    url: `${api_url}/repos/guiinoyaruga/repository-${randomNumber}`,
+                    headers: {
+                        Authorization: `Bearer ${github_token}`,
+                        Accept: 'application/vnd.github+json'
+                    },
+                }).then((response) => {
+                    expect(response.status).to.equal(204)
+                })
+            })
+    });
+
+    it('Não deve ser possivel visualizar o repositório deletado no Github', () => {
+        cy.env(['github_token', 'api_url'])
+            .then(({ github_token, api_url }) => {
+                cy.request({
+                    method: 'GET',
+                    url: `${api_url}/users/guiinoyaruga/repos`,
+                    headers: {
+                        Authorization: `Bearer ${github_token}`,
+                        Accept: 'application/vnd.github+json'
+                    },
+                }).then((response) => {
+                    const repositoryList = response.body.map(r => r.name)
+
+                    expect(response.status).to.equal(200)
+                    expect(repositoryList).not.to.include(`repository-${randomNumber}`)
+                })
+            })
+    });
+});
